373fc2df5f35b61e29cb4a4a68c8e174aa401754
DisnaRef11 <111202315265@mhs.ac.id>
2025-06-18 17:46:53 +0700

    menambahkan api data transaksi pembelian

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -46,8 +46,8 @@
             <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
                 <i class="bi bi-person"></i>
                 <span>Profile</span>
-            </a>
-        </li> <!-- End Profile Nav -->
+             </a>
+        </li><!-- End Profile Nav -->
 
         <li class="nav-item">
             <a class="nav-link <?php echo (uri_string() == 'faq') ? "" : "collapsed" ?>" href="faq">
